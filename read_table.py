0ffe3eb60ed38a104e590882a8f39fd6dce68e9f
chenlin <1chen0bryan@gmail.com>
2024-06-23 10:55:59 -0700

    read_table.py: 分隔符不必有

--- read_table.py
+++ read_table.py
@@ -3,13 +3,13 @@ import sys, csv
 def table_from_file(path: str, delimiter='\t', encoding='utf-8'):
     with open(path, encoding=encoding) as f:
         reader = csv.reader((line for line in
-            f.read().splitlines() if delimiter in line),
+            f.read().splitlines() if line.strip() and not line.startswith('#')),
             delimiter=delimiter)
         return [row for row in reader]
 
 def table_from_stdin(delimiter='\t'):
     reader = csv.reader((line.strip() for
-        line in sys.stdin if delimiter in line),
+        line in sys.stdin if line.strip() and not line.startswith('#')),
         delimiter=delimiter)
     return [row for row in reader]
 
