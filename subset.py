282741a5f269362943e4d6b3828515e02f6b7a87
chenlin <1chen0bryan@gmail.com>
2024-07-23 14:36:33 -0700

    subset.py: 设输出码表一为默认
    -pt, --second-table: 输出码表二（入流）
    
    -d: T1 - T2
    -d + -pt: T2 - T1

--- subset.py
+++ subset.py
@@ -5,23 +5,27 @@ parser = argparse.ArgumentParser()
 parser.add_argument('table1')
 parser.add_argument('table2', nargs='?')
 parser.add_argument('-d', '--difference', action='store_true')
+parser.add_argument('-st', '--second-table', action='store_true')
 args = parser.parse_args()
 
-with open(args.table1, encoding='utf_8') as f:
-    table1 = [line.split('\t') for line in
-        f.read().splitlines()]
-
+table1 = table_from_file(args.table1)
 table2 = from_file_or_stdin(args.table2)
 
-set1 = set(line[0] for line in table1)
-set2 = set(line[0] for line in table2)
+set1 = set(row[0] for row in table1)
+set2 = set(row[0] for row in table2)
 if args.difference:
-    subset = set1.difference(set2)
-    for line in table1:
-        if line[0] in subset:
-            print('\t'.join(line))
+    if args.second_table:
+        subset = set2.difference(set1)
+    else:
+        subset = set1.difference(set2)
 else:
     subset = set1.intersection(set2)
-    for line in table2:
-        if line[0] in subset:
-            print('\t'.join(line))
+
+if args.second_table:
+    output = (row for row in table2)
+else:
+    output = (row for row in table1)
+
+for row in output:
+    if row[0] in subset:
+        print('\t'.join(row))
