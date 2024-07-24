be18296f7ce4b05bfc187f34291add6df166e19e
chenlin <1chen0bryan@gmail.com>
2024-07-24 09:33:55 -0700

    find_duplicate.py: --reverse, 修正重码数算法
    -r, --reverse: 寻找重复出现的字词

--- find_duplicate.py
+++ find_duplicate.py
@@ -22,24 +22,28 @@ def main():
     parser.add_argument('table', nargs='?', default=None)
     parser.add_argument('-pt', '--priority_table', default=None,
         help='排序表：用于排序重码的字')
+    parser.add_argument('-r', '--reverse', action='store_true')
     args = parser.parse_args()
 
     mb = from_file_or_stdin(args.table)
 
+    if args.reverse:
+        mb = [(code, text) for text, code in mb]
+
     if args.priority_table:
-        char_priority = {code:chars for
-            code, chars in table_from_file(args.priority_table)}
+        priority_table = {code:texts for
+            code, texts in table_from_file(args.priority_table)}
     else:
-        char_priority = dict()
+        priority_table = dict()
 
     dup_code = find_dup_code(mb)
-    dup_code = {code: [char for char in chars if not char in char_priority.get(code, '')]
-        for code, chars in dup_code.items()}
-    dup_code = {code: chars for code, chars in dup_code.items() if len(chars) > 1}
+    dup_code = {code: [text for text in texts if not text in priority_table.get(code, '')]
+        for code, texts in dup_code.items()}
+    dup_code = {code: texts for code, texts in dup_code.items() if len(texts) > 1}
 
-    print(f'重码字数：{sum(len("".join(chars)) for chars in dup_code.values())}')
-    for code, chars in dup_code.items():
-        print(f'{code}\t{",".join(chars)}')
+    print(f'重码率：{sum(len(texts) for texts in dup_code.values())}/{len(mb)}')
+    for code, texts in dup_code.items():
+        print(f'{code}\t{",".join(texts)}')
 
 if __name__ == "__main__":
     main()
