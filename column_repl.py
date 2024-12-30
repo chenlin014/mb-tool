f83c1de57b839795db38af5e912b23ae3825db15
chenlin <1chen0bryan@gmail.com>
2024-12-29 22:33:46 -0800

    column_repl.py新选项：-re, --regex（是否使用正则）

--- column_repl.py
+++ column_repl.py
@@ -2,20 +2,25 @@ import re
 from copy import deepcopy
 from read_table import *
 
-def chain_repl(s: str, replmnts: list[str, str]) -> str:
+def chain_repl(s: str, replmnts: list[str, str], regex: bool=False) -> str:
+    if regex:
+        replace = lambda pat, repl, s: re.sub(pat, repl, s)
+    else:
+        replace = lambda pat, repl, s: s.replace(pat, repl)
+
     ns = s
     for pattern, repl in replmnts:
-        ns = re.sub(pattern, repl, ns)
+        ns = replace(pattern, repl, ns)
 
     return ns
 
-def list_chain_repl(ls: list[str], replmnts: list[str, str]) -> list[str]:
-    return [chain_repl(s, replmnts) for s in ls]
+def list_chain_repl(ls: list[str], replmnts: list[str, str], regex: bool=False) -> list[str]:
+    return [chain_repl(s, replmnts, regex) for s in ls]
 
-def column_repl(table: list[list[str]], replmnts: list[str, str], column: int = 0) -> list[list[str]]:
+def column_repl(table: list[list[str]], replmnts: list[str, str], column: int = 0, regex: bool=False) -> list[list[str]]:
     new_table = deepcopy(table)
 
-    new_col = list_chain_repl([row[column] for row in table], replmnts)
+    new_col = list_chain_repl([row[column] for row in table], replmnts, regex)
 
     for i, value in enumerate(new_col):
         new_table[i][column] = value
@@ -29,6 +34,7 @@ def main() -> None:
     parser.add_argument('table', nargs='?', default=None)
     parser.add_argument('-c', '--column', default=0)
     parser.add_argument('-f', '--file', action='store_true')
+    parser.add_argument('-re', '--regex', action='store_true')
     args = parser.parse_args()
 
     if args.file:
@@ -39,7 +45,7 @@ def main() -> None:
 
     args.column = int(args.column)
 
-    new_table = column_repl(from_file_or_stdin(args.table), replmnts, args.column)
+    new_table = column_repl(from_file_or_stdin(args.table), replmnts, args.column, args.regex)
     for row in new_table:
         print('\t'.join(row))
 
