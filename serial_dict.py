253ac71b9228b89bdaf9bd21d3c6e4f1508cf4dc
chenlin <1chen0bryan@gmail.com>
2025-03-21 22:20:45 -0700

    transform.py -> serial_dict.py
    生成郑码类编码

--- serial_dict.py
+++ serial_dict.py
@@ -1,25 +1,19 @@
 import re
 from read_table import *
 
-def simple_transform(table, old2new):
-    for text, code in table:
-        yield (text, ''.join(old2new[c] for c in code))
+def serial_code(code, key4code, rules):
+    for pattern, indexes in rules:
+        if not re.search(pattern, code):
+            continue
 
-def transform_w_rule(table, old2new, rules):
-    for text, code in table:
-        for pattern, indexes in rules:
-            if not re.match(pattern, code):
+        ncode = ''
+        for ind1, ind2 in indexes:
+            try:
+                ncode += key4code[code[ind1]][ind2]
+            except:
                 continue
 
-            ncode = ''
-            for ind1, ind2 in indexes:
-                try:
-                    ncode += old2new[code[ind1]][ind2]
-                except:
-                    continue
-
-            yield (text, ncode)
-            break;
+        return ncode
 
 LATIN_UPPER = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 latin_lower = 'abcdefghijklmnopqrstuvwxyz'
@@ -46,26 +40,29 @@ def main():
     from common import common_argparser
 
     parser = common_argparser()
-    parser.add_argument('old2new', help='旧码对新码')
+    parser.add_argument('code_key_map', help='码键映射')
     parser.add_argument('table', nargs='?', help='码表', default=None)
     parser.add_argument('-r', '--rule_table', help='取码规则文件', default=None)
     args = parser.parse_args()
 
-    old2new = {old:new for old, new in
-        read_table(args.old2new, args.delimiter)}
+    key4code = {code:key for code, key in
+        read_table(args.code_key_map, args.delimiter)}
 
-    table = read_table(args.table, args.delimiter)
+    table = ((text, code) for text, code in
+        read_table(args.table, args.delimiter))
 
     if not args.rule_table:
-        for text, code in simple_transform(table, old2new):
-            print(f'{text}\t{code}')
+        for text, code in table:
+            keys = ''.join(key4code[c] for c in code)
+            print(f'{word}\t{keys}')
         quit()
 
     rules = [(pattern, parseMethod(method)) for pattern, method in
         read_table(args.rule_table, args.delimiter)]
 
-    for text, code in transform_w_rule(table, old2new, rules):
-        print(f'{text}\t{code}')
+    for text, code in table:
+        ncode = serial_code(code, key4code, rules)
+        print(f'{text}\t{ncode}')
 
 if __name__ == '__main__':
     main()
