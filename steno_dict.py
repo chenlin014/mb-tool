9698244858f6aecad001ab811c8d7bc209da4310
chenlin <1chen0bryan@gmail.com>
2025-03-26 17:45:09 -0700

    steno_dict.py: 修正acts_to_chord, --no-table
    --no-table
    不用码表、用并击表当码表。

--- steno_dict.py
+++ steno_dict.py
@@ -55,42 +55,51 @@ def keys_to_chord(keys, order):
     return ''.join(chord)
 
 def acts_to_chord(actions, system, act2rows=ACT_TO_ROWS, onLeft=True):
-    keys_to_chord(
-        acts_to_keys(actions, system, act2rows, onLeft)
+    return keys_to_chord(
+        acts_to_keys(actions, system, act2rows, onLeft),
+        system['key_order']
     )
 
 def main():
     parser = common_argparser()
     parser.add_argument('system', help='并击系统')
-    parser.add_argument('chordmap', help='字根并击表')
+    parser.add_argument('chordmap', help='并击表')
     parser.add_argument('table', nargs='?', help='码表', default=None)
+    parser.add_argument('-nt', '--no-table', action='store_true')
     args = parser.parse_args()
 
     with open(args.system) as f:
         system = json.loads(f.read())
     system['key_order'] = {key: i for i, key in enumerate(system['key_order'])}
 
-    code2keys = {code:(acts_to_keys(acts, system, onLeft=True),
-                     acts_to_keys(acts, system, onLeft=False)) for code, acts in
-                    read_table(args.chordmap, delimiter=args.delimiter)}
+    if args.no_table:
+        codes2chords = lambda codes: [acts_to_chord(acts, system, onLeft=(i % 2 == 0))
+                                        for i, acts in enumerate(codes.split(' '))
+                                      ]
+        table = read_table(args.chordmap, args.delimiter)
+    else:
+        code2keys = {code:(acts_to_keys(acts, system, onLeft=True),
+                         acts_to_keys(acts, system, onLeft=False)) for code, acts in
+                        read_table(args.chordmap, delimiter=args.delimiter)}
 
-    table = read_table(args.table, args.delimiter)
+        codes2chords = lambda codes: [keys_to_chord(
+            set(key
+                for code in part
+                    for key in code2keys[code][i%2]
+                ), system['key_order'])
+            for i, part in enumerate(codes.split(' '))]
+
+        table = read_table(args.table, args.delimiter)
 
     for text, codes in table:
         if re.match(r'\{.+\}', codes):
-            chords = [apply_keymap(chord, system, onLeft=(i%2 == 0)) for i, chord in
-                enumerate(codes[1:-1].split(','))]
+            chords = [acts_to_chord(acts, system, onLeft=(i%2 == 0))
+                for i, acts in enumerate(codes[1:-1].split(','))]
             codes = ''
         else:
             chords = []
 
-        for i, part in enumerate(codes.split(' ')):
-            keys = set()
-            for code in part:
-                keys = keys | code2keys[code][i%2]
-
-            chord = keys_to_chord(keys, system['key_order'])
-            chords.append(''.join(chord))
+        chords = codes2chords(codes)
 
         strokes = [(lchord, rchord) for lchord, rchord in zip(chords[::2], chords[1::2])]
 
